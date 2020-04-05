ee3a47c76400d06db71234a13ccaa5df14250ba6
Voyager <startcev-german@mail.ru>
2020-04-05 22:34:47 +0300

    twig applying

--- www/index.php
+++ www/index.php
@@ -1,5 +1,5 @@
 <?php
-	require_once __DIR__ . '/vendor/autoload.php';
+	require_once __DIR__ . '/../vendor/autoload.php';
 	$loader = new Twig_Loader_Filesystem('/templates');
 	$twig = new Twig_Environment($laoder);
 ?>
