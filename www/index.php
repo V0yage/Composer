9e6dc654c812ad5033e916032bf0f838cd1ce601
Voyager <startcev-german@mail.ru>
2020-04-05 22:37:02 +0300

    twig applying

--- www/index.php
+++ www/index.php
@@ -1,7 +1,7 @@
 <?php
 	require_once __DIR__ . '/../vendor/autoload.php';
-	//$loader = new Twig_Loader_Filesystem('/templates');
-	//$twig = new Twig_Environment($laoder);
+	$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
+	$twig = new Twig_Environment($laoder);
 ?>
 
 <!DOCTYPE html>
